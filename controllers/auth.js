3aaa4b3595b9a7ecc9ad3a532cdc3106b9955332
Emmanuel Zelarayan <emmavz@live.com>
2022-07-25 17:02:33 -0300

    Initial files. Routes, controllers and models creation

---
+++ controllers/auth.js
@@ -0,0 +1,16 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ user });
+};
+
+const login = async (req, res) => {
+  res.send("Login user");
+};
+
+module.exports = {
+  register,
+  login,
+};
